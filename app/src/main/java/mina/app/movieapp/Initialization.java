a62ae2ad168ddd18608ca649751a9d80fe38974e
Levi Horst <levi.horst@bruins.belmont.edu>
2023-11-28 12:49:35 -0600

    added Initialization activity

---
+++ app/src/main/java/mina/app/movieapp/Initialization.java
@@ -0,0 +1,14 @@
+package mina.app.movieapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Initialization extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_initialization);
+    }
+}
