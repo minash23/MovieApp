59d8d67571b787baa9dbc8a088bdde09f665aa8a
mina <minas27188@gmail.com>
2023-11-28 12:32:31 -0600

    Initial commit

---
+++ app/src/main/java/mina/app/movieapp/logInActivity.java
@@ -0,0 +1,14 @@
+package mina.app.movieapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class logInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+    }
+}
