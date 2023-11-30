7597e644c1eb58d5865ffcd8bb3fbaedfc8a9369
Daniel5263 <dany9000@gmail.com>
2023-11-30 12:59:44 -0600

    main_activty.xml added Setup main_activity.xml layout

--- app/src/main/java/mina/app/movieapp/MainActivity.java
+++ app/src/main/java/mina/app/movieapp/MainActivity.java
@@ -1,8 +1,11 @@
 package mina.app.movieapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void storeAccountInFirebase(User user) {
+        String userName = user.getName();
+
+        databaseReference.orderByChild("name").equalTo(userName).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    Toast.makeText(getApplicationContext(), userName + " already stored in database", Toast.LENGTH_SHORT).show();
+                } else {
+                    String key = databaseReference.push().getKey();
+                    databaseReference.child(key).setValue(user);
+                    Toast.makeText(getApplicationContext(), userName + " added!", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.e("Firebase", "Error checking duplicate", databaseError.toException());
+            }
+        });
     }
 }
