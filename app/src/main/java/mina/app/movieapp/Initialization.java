44674be59ffbe664c28ee0fb86a8f791dd6e26b1
mina <minas27188@gmail.com>
2023-12-09 10:18:47 -0600

    Main Activity Functionality

--- app/src/main/java/mina/app/movieapp/Initialization.java
+++ app/src/main/java/mina/app/movieapp/Initialization.java
@@ -41,7 +41,6 @@ public class Initialization extends AppCompatActivity {
         }
     };
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
