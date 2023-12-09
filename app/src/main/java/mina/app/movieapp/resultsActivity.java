9e8a1c88b0b5424fc456bb35a6b210c273a14bc0
Daniel5263 <dany9000@gmail.com>
2023-12-09 10:54:18 -0600

    results

--- app/src/main/java/mina/app/movieapp/resultsActivity.java
+++ app/src/main/java/mina/app/movieapp/resultsActivity.java
@@ -10,6 +10,8 @@ import com.androidnetworking.common.ANRequest;
 import com.androidnetworking.common.Priority;
 import com.androidnetworking.error.ANError;
 import android.widget.ImageView;
+import android.widget.TextView;
+
 import com.squareup.picasso.Picasso;
 import com.androidnetworking.interfaces.ParsedRequestListener;
 
@@ -27,14 +29,16 @@ public class resultsActivity extends AppCompatActivity {
 
     private void makeRequest(String id){
         ANRequest req = AndroidNetworking.get("https://api.themoviedb.org/3/movie/{id}?api_key=dbc8826d07a32ea3f193cf0541626b5e")
-                        .addPathParameter("name", id)
+                        .addPathParameter("id", id)
                         .setPriority(Priority.LOW)
                         .build();
         req.getAsJSONObject(requestListener);
 
-                String imageURL = "https://api.themoviedb.org/3/movie/" + getId() + "jpg";
+                String imageURL = "https://api.themoviedb.org/3/movie/" + getPoster_path() + "jpg";
 
                 Picasso.get().load(imageURL).into(imageView);
+
+                ((TextView) findViewById(R.id.T1)).setText(getTitle());
 
         //https://api.themoviedb.org/3/search/movie?query={Movie Name}&api_key=dbc8826d07a32ea3f193cf0541626b5e
     }
