637040f89933d84875dcc0d8f18c89f54f030dff
Daniel5263 <dany9000@gmail.com>
2023-12-09 11:29:54 -0600

    results

--- app/src/main/java/mina/app/movieapp/resultsActivity.java
+++ app/src/main/java/mina/app/movieapp/resultsActivity.java
@@ -12,35 +12,58 @@ import com.androidnetworking.error.ANError;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.androidnetworking.interfaces.JSONObjectRequestListener;
 import com.squareup.picasso.Picasso;
 import com.androidnetworking.interfaces.ParsedRequestListener;
 
+import org.json.JSONObject;
+
+import java.lang.reflect.Array;
+
 public class resultsActivity extends AppCompatActivity {
 
+    String value;
+    String[] array;
     ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
+        Bundle extras = getIntent().getExtras();
+        if(extras != null){
+            value = extras.getString("vibe");
+            array = extras.getStringArray("ArrayOfButtons");
+
+        }
 
         imageView = findViewById(R.id.I1);
+
+
+
+        //String imageURL = "https://api.themoviedb.org/3/movie/" + getPoster_path() + "jpg";
+        //Picasso.get().load(imageURL).into(imageView);
+       // ((TextView) findViewById(R.id.T1)).setText(getTitle());
     }
 
-    private void makeRequest(String id){
-        ANRequest req = AndroidNetworking.get("https://api.themoviedb.org/3/movie/{id}?api_key=dbc8826d07a32ea3f193cf0541626b5e")
-                        .addPathParameter("id", id)
-                        .setPriority(Priority.LOW)
-                        .build();
-        req.getAsJSONObject(requestListener);
+    JSONObjectRequestListener requestListener = new JSONObjectRequestListener() {
+        @Override
+        public void onResponse(JSONObject response) {
 
-                String imageURL = "https://api.themoviedb.org/3/movie/" + getPoster_path() + "jpg";
+        }
 
-                Picasso.get().load(imageURL).into(imageView);
+        @Override
+        public void onError(ANError anError) {
 
-                ((TextView) findViewById(R.id.T1)).setText(getTitle());
-
+        }
+    };
+    private void makeRequest(String id) {
         //https://api.themoviedb.org/3/search/movie?query={Movie Name}&api_key=dbc8826d07a32ea3f193cf0541626b5e
+        ANRequest req = AndroidNetworking.get("https://api.themoviedb.org/3/movie/{id}?api_key=dbc8826d07a32ea3f193cf0541626b5e")
+                .addPathParameter("id", id)
+                .setPriority(Priority.LOW)
+                .build();
+        req.getAsJSONObject(requestListener);
     }
 
 }
