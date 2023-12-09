f4f95f07d8d3c2d42ddb8ebdbcaac94baa411408
mina <minas27188@gmail.com>
2023-12-08 20:17:46 -0600

    Adds Initialization Check in logInActivity

--- app/src/main/java/mina/app/movieapp/MainActivity.java
+++ app/src/main/java/mina/app/movieapp/MainActivity.java
@@ -14,6 +14,7 @@ import android.util.Log;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,33 +22,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         FirebaseDatabase database = FirebaseDatabase.getInstance();
-
         databaseReference = FirebaseDatabase.getInstance().getReference();
-        //storeUserInFirebase(data);
     }
 
-    //Account checker below
-    private DatabaseReference databaseReference;
 
-    private void storeUserInFirebase(User user) {
-        String userKey = user.getEmail();
 
-        databaseReference.orderByChild("email").equalTo(userKey).addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                if (dataSnapshot.exists()) {
-                    Toast.makeText(getApplicationContext(), userKey + " already stored in database", Toast.LENGTH_SHORT).show();
-                } else {
-                    String key = databaseReference.push().getKey();
-                    databaseReference.child(key).setValue(user);
-                    Toast.makeText(getApplicationContext(), userKey + " added!", Toast.LENGTH_SHORT).show();
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-                Log.e("Firebase", "Error checking duplicate", databaseError.toException());
-            }
-        });
-    }
 }
--- app/src/main/java/mina/app/movieapp/logInActivity.java
+++ app/src/main/java/mina/app/movieapp/logInActivity.java
@@ -1,8 +1,5 @@
 package mina.app.movieapp;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,26 +7,34 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class logInActivity extends AppCompatActivity {
+    Boolean initializedValue = false;
     Button logInButton;
     Button SignUpButton;
     EditText userInput;
     EditText passInput;
     FirebaseUser user;
+
     View.OnClickListener logListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             signInAccount(userInput.getText().toString().trim(), passInput.getText().toString().trim());
         }
     };
+
     View.OnClickListener signListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
@@ -38,26 +43,8 @@ public class logInActivity extends AppCompatActivity {
         }
     };
 
-    public void signInAccount(String email, String password){
-        mAuth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d("Hi", "signInWithEmail:success");
-                            user = mAuth.getCurrentUser();
-                            Intent intent = new Intent(getApplicationContext(), Initialization.class);
-                            startActivity(intent);
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w("Hi", "signInWithEmail:failure", task.getException());
-                            Toast.makeText(getApplicationContext(), "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-                            //updateUI(null);
-                        }
-                    }
-                });
+    public interface InitializedValueCallback {
+        void onInitializedValue(boolean initializedValue);
     }
 
     private FirebaseAuth mAuth;
@@ -71,11 +58,85 @@ public class logInActivity extends AppCompatActivity {
         userInput = findViewById(R.id.username_input);
         SignUpButton = findViewById(R.id.sign_up_button);
 
-
         // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance();
         logInButton.setOnClickListener(logListener);
         SignUpButton.setOnClickListener(signListener);
+    }
 
+    public void signInAccount(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            String email = mAuth.getCurrentUser().getEmail().toString();
+                            getInitializedValue(email, new InitializedValueCallback() {
+                                @Override
+                                public void onInitializedValue(boolean initializedValue) {
+                                    Log.d("Final Initialized Value", String.valueOf(initializedValue));
+
+                                    if (initializedValue) {
+                                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                        startActivity(intent);
+                                    } else {
+                                        Intent intent = new Intent(getApplicationContext(), Initialization.class);
+                                        startActivity(intent);
+                                    }
+                                }
+                            });
+                        } else {
+                            Log.w("Hi", "signInWithEmail:failure", task.getException());
+                            Toast.makeText(getApplicationContext(), "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    public void getInitializedValue(String userEmail, InitializedValueCallback callback) {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference usersRef = database.getReference("/Users");
+
+        usersRef.orderByChild("email").equalTo(userEmail).addListenerForSingleValueEvent(
+                new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.exists()) {
+                            String userKey = dataSnapshot.getChildren().iterator().next().getKey();
+                            DatabaseReference userRef = FirebaseDatabase.getInstance().getReference("/Users/" + userKey);
+
+                            userRef.child("initialized").addListenerForSingleValueEvent(
+                                    new ValueEventListener() {
+                                        @Override
+                                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                            if (dataSnapshot.exists()) {
+                                                boolean initializedValue = dataSnapshot.getValue(Boolean.class);
+                                                Log.d("Initialized Here?", String.valueOf(initializedValue));
+                                                callback.onInitializedValue(initializedValue);
+                                            } else {
+                                                Log.d("Initialized", "Value not found");
+                                                callback.onInitializedValue(false);
+                                            }
+                                        }
+
+                                        @Override
+                                        public void onCancelled(@NonNull DatabaseError databaseError) {
+                                            Log.e("Firebase", "Error retrieving initialized value: " + databaseError.getMessage());
+                                            callback.onInitializedValue(false);
+                                        }
+                                    });
+                        } else {
+                            Log.d("User", "User not found");
+                            callback.onInitializedValue(false);
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                        Log.e("Firebase", "Error querying users: " + databaseError.getMessage());
+                        callback.onInitializedValue(false);
+                    }
+                });
     }
 }
