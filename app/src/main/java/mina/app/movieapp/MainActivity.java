e5da8302e0445c5a2d324d623a95f678c5e93411
Daniel5263 <dany9000@gmail.com>
2023-12-09 09:45:41 -0600

    Button ids added

--- app/src/main/java/mina/app/movieapp/MainActivity.java
+++ app/src/main/java/mina/app/movieapp/MainActivity.java
@@ -9,45 +9,33 @@ import com.google.firebase.database.ValueEventListener;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton searchButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        searchButton = findViewById(R.id.button1);
 
-        databaseReference = FirebaseDatabase.getInstance().getReference();
-        //storeUserInFirebase(data);
-    }
-
-    //Account checker below
-    private DatabaseReference databaseReference;
-
-    private void storeUserInFirebase(User user) {
-        String userKey = user.getEmail();
-
-        databaseReference.orderByChild("email").equalTo(userKey).addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                if (dataSnapshot.exists()) {
-                    Toast.makeText(getApplicationContext(), userKey + " already stored in database", Toast.LENGTH_SHORT).show();
-                } else {
-                    String key = databaseReference.push().getKey();
-                    databaseReference.child(key).setValue(user);
-                    Toast.makeText(getApplicationContext(), userKey + " added!", Toast.LENGTH_SHORT).show();
-                }
-            }
+        searchButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-                Log.e("Firebase", "Error checking duplicate", databaseError.toException());
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, resultsActivity.class);
+                        startActivity(intent);
             }
         });
     }
+
 }
