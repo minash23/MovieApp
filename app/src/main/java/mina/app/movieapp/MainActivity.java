c9cf4f60edc72a93a0671a3cdfa4700540eabc3b
Daniel5263 <dany9000@gmail.com>
2023-12-09 10:20:24 -0600

    results

--- app/src/main/java/mina/app/movieapp/MainActivity.java
+++ app/src/main/java/mina/app/movieapp/MainActivity.java
@@ -36,6 +36,8 @@ public class MainActivity extends AppCompatActivity {
                         startActivity(intent);
             }
         });
+
+        
     }
 
 }
--- app/src/main/java/mina/app/movieapp/resultsActivity.java
+++ app/src/main/java/mina/app/movieapp/resultsActivity.java
@@ -3,12 +3,40 @@ package mina.app.movieapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.ANRequest;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+import android.widget.ImageView;
+import com.squareup.picasso.Picasso;
+import com.androidnetworking.interfaces.ParsedRequestListener;
 
 public class resultsActivity extends AppCompatActivity {
+
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
+
+        imageView = findViewById(R.id.I1);
     }
+
+    private void makeRequest(String id){
+        ANRequest req = AndroidNetworking.get("https://api.themoviedb.org/3/movie/{id}?api_key=dbc8826d07a32ea3f193cf0541626b5e")
+                        .addPathParameter("name", id)
+                        .setPriority(Priority.LOW)
+                        .build();
+        req.getAsJSONObject(requestListener);
+
+                String imageURL = "https://api.themoviedb.org/3/movie/" + getId() + "jpg";
+
+                Picasso.get().load(imageURL).into(imageView);
+
+        //https://api.themoviedb.org/3/search/movie?query={Movie Name}&api_key=dbc8826d07a32ea3f193cf0541626b5e
+    }
+
 }
